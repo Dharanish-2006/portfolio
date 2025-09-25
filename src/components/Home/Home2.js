5806bfce89f3a48bbd7c003c03338ef39106b48e
dharanish <dharanishwar.2006@gmail.com>
2025-09-25 04:10:57 -1200

    contact update

--- src/components/Home/Home2.js
+++ src/components/Home/Home2.js
@@ -90,7 +90,7 @@ function Home2() {
                   rel="noreferrer"
                   className="icon-colour home-social-icons"
                 >
-                  <AiFilllLinkedin/>
+                  <AiFillLinkedin/>
                 </a>
               </li>
             </ul>
